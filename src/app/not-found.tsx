5ad7c3a5db794174c73a5345da766e105344e968
gwonyejun <kyjuni1059@gmail.com>
2024-03-19 16:03:27 +0900

    참거짓

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,11 @@
+import Navigation from "../../components/Navigation"
+
 export default function NotFoundError(){
 
   
   return (
     <>
+    <Navigation />
     <h1>접근 할 수 없습니다.</h1>
     </>
   )
--- src/app/two/page.tsx
+++ src/app/two/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
     <Navigation />
- <h1>two</h1>
+
     </>
   );
 }
